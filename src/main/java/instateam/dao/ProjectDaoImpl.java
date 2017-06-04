631dca0b16ed627c436cb9d6d21bc5a850200e60
mark <ulatec@gmail.com>
2017-06-03 17:36:10 -0700

    groundwork for ProjectDaoImpl

--- src/main/java/instateam/dao/ProjectDaoImpl.java
+++ src/main/java/instateam/dao/ProjectDaoImpl.java
@@ -2,29 +2,39 @@ package instateam.dao;
 
 import java.util.List;
 import instateam.model.Project;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.criteria.CriteriaBuilder;
 
 /**
  * Created by mark on 5/31/2017.
  */
 public class ProjectDaoImpl implements ProjectDao{
-
+  @Autowired
+  private SessionFactory sessionFactory;
   @Override
   public List<Project> findAll() {
+    Session session = sessionFactory.openSession();
+
+    session.close();
     return null;
   }
 
   @Override
   public Project findById(Long id) {
+    Session session = sessionFactory.openSession();
     return null;
   }
 
   @Override
   public void save(Project project) {
-
+    sessionFactory.openSession();
   }
 
   @Override
   public void delete(Project project) {
-
+    Session session = sessionFactory.openSession();
   }
 }
