fdedd3dfd1502f07a781c93072ba312a9b3d680c
mark <ulatec@gmail.com>
2017-07-09 07:19:43 -0700

    Create stubs for Role service/dao

--- src/main/java/instateam/service/RoleService.java
+++ src/main/java/instateam/service/RoleService.java
@@ -1,7 +1,15 @@
 package instateam.service;
 
+import instateam.model.Role;
+
+import java.util.List;
+
 /**
  * Created by mark on 7/9/2017.
  */
 public interface RoleService {
+    List<Role> findAll();
+    Role findById(Long id);
+    void save(Role role);
+    void delete(Role role);
 }
--- src/main/java/instateam/service/RoleServiceImpl.java
+++ src/main/java/instateam/service/RoleServiceImpl.java
@@ -1,7 +1,31 @@
 package instateam.service;
 
+import instateam.model.Role;
+
+import java.util.List;
+
 /**
  * Created by mark on 7/9/2017.
  */
-public class RoleServiceImpl {
+public class RoleServiceImpl implements RoleService{
+
+    @Override
+    public List<Role> findAll() {
+        return null;
+    }
+
+    @Override
+    public Role findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Role role) {
+
+    }
+
+    @Override
+    public void delete(Role role) {
+
+    }
 }
